6b2e1e91ba9cad999b1cca57a352ebcbf2500ef2
b.lopez2 <114659117+blopez2@users.noreply.github.com>
2023-12-08 00:07:05 +0100

    Update and rename bintree.txt to bintree.ml

---
+++ PP/P9/bintree.ml
@@ -0,0 +1,24 @@
+let rec in_order tree =
+  match tree with
+  | Empty -> []
+  | Node (value, left, right) -> in_order left @ [value] @ in_order right
+
+
+let rec insert ord tree x =
+  match tree with
+  | Empty -> Node (x, Empty, Empty)
+  | Node (value, left, right) ->
+      if ord x value then Node (value, insert ord left x, right)
+      else Node (value, left, insert ord right x)
+
+
+let bst ord l =
+  List.fold_left (fun acc x -> insert ord acc x) Empty l
+
+
+let rec qsort ord lst =
+  match lst with
+  | [] -> []
+  | pivot :: rest ->
+      let smaller, larger = List.partition (fun x -> ord x pivot) rest in
+      qsort ord larger @ [pivot] @ qsort ord smaller
